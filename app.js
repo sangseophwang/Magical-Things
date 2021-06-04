d95102ad850856c666b8a95b00750df572f5731d
SEOP <sshwang12345@gmail.com>
2021-06-04 23:31:52 +0900

    Created Momentum_clone!

---
+++ app.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "Name";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.innerText = `Welcome, ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedName = localStorage.getItem(USERNAME_KEY);
+
+if (savedName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedName);
+}
