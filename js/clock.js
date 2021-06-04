f80c9a4103ae7bf0a5e131cde97ae603a00a6a7b
SEOP <sshwang12345@gmail.com>
2021-06-05 00:43:07 +0900

    Updated clock

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector(".clock h2");
+
+function getTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
