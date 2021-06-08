d6d3ef074bbfa03c1a7279a67e620f4e45183699
SEOP <sshwang12345@gmail.com>
2021-06-08 13:42:11 +0900

    Updated Todo-list

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,6 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
+const days = document.querySelector(".days");
 
 function getHour() {
   const date = new Date();
@@ -13,7 +14,19 @@ function getMinute() {
   minute.innerText = `${minutes}`;
 }
 
+function getDays() {
+  const date = new Date();
+  const weekName = new Array("일", "월", "화", "수", "목", "금", "토");
+  const month = String(date.getMonth() + 1);
+  const day = String(date.getDate());
+  const yoil = String(weekName[date.getDay()]);
+
+  days.innerText = `${month}월 ${day}일 ${yoil}요일`;
+}
+
 getHour();
 getMinute();
+getDays();
 setInterval(getHour, 1000);
 setInterval(getMinute, 1000);
+setInterval(getDays, 1000);
--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function painToDo(todo) {
+function paintToDo(todo) {
   const list = document.createElement("li");
   const span = document.createElement("span");
+  list.appendChild(span);
+  span.innerHTML = todo;
+  toDoList.appendChild(list);
 }
 
 function handleToDoSubmit(event) {
